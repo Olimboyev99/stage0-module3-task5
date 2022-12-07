36e9e8e75c88b3a38a0fb09bc63050cbb822b10d
Hasanboy <hasanboyolimboyev40@gmail.com>
2022-12-07 15:26:59 +0500

    5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.task5;
 
+import java.util.Scanner;
+
 public class NumberDivider {
     void divide(int toBeDivided) {
+        Scanner in = new Scanner(System.in);
+        toBeDivided = in.nextInt();
+        int d=toBeDivided/5;
+
     }
 }
