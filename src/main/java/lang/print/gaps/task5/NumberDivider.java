7f9e1bebea5e5351cbe0e200a3fec7f4f4e18663
Hasanboy <hasanboyolimboyev40@gmail.com>
2022-12-07 15:45:54 +0500

    5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,12 +1,6 @@
 package lang.print.gaps.task5;
 
-import java.util.Scanner;
-
 public class NumberDivider {
     void divide(int toBeDivided) {
-        Scanner in = new Scanner(System.in);
-        toBeDivided = in.nextInt();
-        int d=toBeDivided/5;
-
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,13 +1,6 @@
 package lang.print.gaps.task5;
 
-import java.util.Scanner;
-
 public class TimeConvertor {
-
     public void convert(float minutes) {
-        Scanner in = new Scanner(System.in);
-        minutes = in.nextFloat();
-        float s = 60*minutes;
-        System.out.println(s);
     }
 }
